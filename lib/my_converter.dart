d42e839cd9802a14a30934c8ebc620e63834bc7a
gdgalvis <73660713+gdgalvis@users.noreply.github.com>
2023-08-02 16:24:26 +0000

    prueba

--- lib/my_converter.dart
+++ lib/my_converter.dart
@@ -7,38 +7,38 @@ class MyConverter {
 
   MyConverter() {
     // todo: Inicializa [_binary] y [_decimal] en ['0']
-    this._binary = 0;
-    this._decimal = 0;
-
+    this._binary = "0";
+    this._decimal = "0";
   }
 
   // TODO: Crea un [setter] y [getter] para [binary] .
-  void set _binary(String bins) {
+  void set binary(String bins) {
     _binary = bins;
   }
 
-  String get _binary(){
+  String get binary {
     return _binary;
   }
 
-
   // TODO: Crea un [setter] y [getter] para [decimal].
-  void set _decima(String decs) {
+  void set decima(String decs) {
     _decimal = decs;
   }
-  String get _decimal(){
+
+  String get decimal {
     return _decimal;
   }
 
   String convertBinary() {
     // todo: Usa [Util] para convertir el numero binario a un numero decimal.
-    String result = bin2dec(String _binary) ;
+    String result = Utils.bin2dec(_binary);
     return result;
   }
 
   String convertDecimal() {
     // todo: Usa [Converter] para convertir el numero decimal a un numero binario.
-    String result = dec2bin(String _decimal) ;
+    String result = Utils.dec2bin(_decimal);
     return result;
   }
 }
+
